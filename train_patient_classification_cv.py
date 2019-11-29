650db661524e024b126c5ed8baeb90b9ea9d701f
huhrichard <richardpokard@gmail.com>
2019-11-29 02:00:05 -0500

    lower resolution for memory problem

--- train_patient_classification_cv.py
+++ train_patient_classification_cv.py
@@ -37,7 +37,7 @@ args = parser.parse_args()
 print(args)
 
 # print("# Batch: ",)
-input_tensor_size = (800, 800)
+input_tensor_size = (500, 500)
 
 
 def model_training_and_evaluate_testing(epochs,
