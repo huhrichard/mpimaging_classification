75e315b3acc558f3f348b2f7dea800fd67c024cd
huhrichard <richardpokard@gmail.com>
2020-01-04 01:01:56 -0500

    1e-6

--- utils/configs_4C.py
+++ utils/configs_4C.py
@@ -29,7 +29,7 @@ parameters_grid = {
                    # "p_model": ["wide_resnet101_2"],
                    "p_weight": [True],
                    "feat_ext": [False],
-                   "lr": [1e-7],
+                   "lr": [1e-6],
                    "wd": [1e-3],
                    "input_dim": [(4, 224, 224)],
                    "out_list": [False],
