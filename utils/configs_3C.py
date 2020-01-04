25c154a314545e079306a3ce3be0856ab837e75d
huhrichard <richardpokard@gmail.com>
2020-01-04 14:26:43 -0500

    no message

--- utils/configs_3C.py
+++ utils/configs_3C.py
@@ -25,7 +25,7 @@ parameters_grid = {
                    # "performance_metrics_list": [metric_list],
                    # "device": [device],
                    # "p_model": ["resnext101_32x8d"],
-                   "p_model": ["resnet18"],
+                   "p_model": ["resnet50"],
                    # "p_model": ["wide_resnet101_2"],
                    "p_weight": [True],
                    "feat_ext": [False],
@@ -33,7 +33,7 @@ parameters_grid = {
                    "wd": [1e-3],
                    "input_dim": [(3, 224, 224)],
                    "out_list": [False],
-                   "loss": ["FL"],
+                   "loss": ["BCE"],
                    "train_data_normal": [True],
                    "n_batch": [5],
                    }
