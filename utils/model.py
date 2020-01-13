1f59298f204769f932ddacb42e528fb54c51a5dc
huhrichard <richardpokard@gmail.com>
2020-01-13 14:18:38 -0500

    no message

--- utils/model.py
+++ utils/model.py
@@ -171,7 +171,7 @@ def get_pretrained_net(model_name, input_size, module_prefix=None, pretrain_weig
             pretrained_net = getattr(seg, model_name)(pretrained=pretrain_weight)
 
         set_parameter_requires_grad(pretrained_net, feature_extracting)
-        test_input = torch.zeros(1, 3, input_size[1], input_size[2])
+        test_input = torch.zeros(1, 3, input_size[-2], input_size[-1])
         # TODO: get the only layers want from 'modules_employing',
         # print(model_name)
         if "res" in model_name and "fcn" not in model_name:
