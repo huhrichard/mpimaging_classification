bd65cdeb36bd3e1f3964a0e1a2288aa04743ff0c
huhrichard <richardpokard@gmail.com>
2020-01-13 19:27:18 -0500

    no message

--- train_patient_classification_simplest_cv.py
+++ train_patient_classification_simplest_cv.py
@@ -267,7 +267,7 @@ if __name__ == "__main__":
                                               metrics=metrics,
                                               state='val',
                                               out_label=label_name,
-                                              params_idx=args.params_idx,
+                                              params_idx=params_picked[0],
                                               nth_fold=args.nth_fold)
 
     score_df.to_csv(score_csv_name, index=None, header=True)
